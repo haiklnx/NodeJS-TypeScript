f6a41d937f3001f7c458c903fe15ff08050098c6
haiklnx <9haik1@gmail.com>
2021-01-15 20:51:03 -0300

    Adicionando Aula22

---
+++ Curso/Aula22/index.js
@@ -0,0 +1,15 @@
+/* 
+operadores lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+console.log(true && true); // true vai retornar quando todas são verdadeiras
+console.log(true && false); 
+console.log(true || false); // true quando alguma expressão voltar verdadeira
+console.log(false || false);
+
+const not = !true; // retorna o inverso do resultado, se for verdadeiro vai retornar falso
+
+console.log(not);
