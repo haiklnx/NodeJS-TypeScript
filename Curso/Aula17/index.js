3156c7de920bf33e6c6da631cde58d58741eac29
haiklnx <9haik1@gmail.com>
2021-01-15 20:11:51 -0300

    Adicionando aula 17

---
+++ Curso/Aula17/index.js
@@ -0,0 +1,19 @@
+const raiz = n => n ** 0.5;
+
+
+// const raiz = (n) => {
+//     return n ** 0.5;
+// };
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+// function soma(x, y) {
+//     return x + y;
+//     console.log('ola JS'); // não vai ser executado nenhum código após return
+// }
+
+// console.log(soma(3,5));
+// console.log(soma(7,3));
+
