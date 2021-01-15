dbbbf306abba49cbc88054e0cc7330acd6a8d78c
haiklnx <9haik1@gmail.com>
2021-01-15 20:12:20 -0300

    Adicionando aula 21

---
+++ Curso/Aula21/index.js
@@ -0,0 +1,18 @@
+const comp = 5 > 3;
+const comp2 = 2 >= 3;
+const comp3 = 11 <= 8;
+const comp4 = 13 >= 13;
+const comp5 = 4 != 3;
+const comp6 = comp == comp2;
+const comp7 = 10 == '10'; //true
+const comp8 = 10 === '10'; // false - sempre utilizar === pois vai checar tipo e valor
+
+
+console.log(comp);
+console.log(comp2);
+console.log(comp3);
+console.log(comp4);
+console.log(comp5);
+console.log(comp6);
+console.log(comp7);
+console.log(comp8);
