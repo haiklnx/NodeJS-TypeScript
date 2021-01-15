e18d4192f76ab62aac3e79b978d2b1a7d9ab17c3
haiklnx <9haik1@gmail.com>
2021-01-15 20:11:36 -0300

    Adicionando aula 16

---
+++ Curso/Aula16/index.js
@@ -0,0 +1,24 @@
+const alunos = ['gabriel', 'maria', 'joao'];
+
+
+console.log(typeof alunos);
+
+
+// alunos.push('zé');
+// alunos.push('ryu');
+// console.log(alunos.slice(3,5))
+// console.log(alunos.slice(0,-1))
+// delete alunos[1];
+// alunos.shift();
+// const removido = alunos.pop();
+// console.log(removido);
+// alunos.unshift('Vai para no indice 1');
+// alunos.unshift('Vou aparecer na primeira posição');
+// alunos.push('1');
+// alunos.push('2');
+// alunos.push('3');
+// alunos[0] = 'estrela'
+// alunos[alunos.length] = 'dog'
+// alunos[alunos.length] = 'asdsa'
+// console.log(alunos[0]);
+// console.log(alunos[2]);
