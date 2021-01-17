dd54edb5d8e80d3dfd89a73ed837b1737a811072
haiklnx <9haik1@gmail.com>
2021-01-17 16:49:37 -0300

    Add escopo de funções Aula31

---
+++ Curso/Aula31/index.js
@@ -0,0 +1,18 @@
+let nome = 'gabriel';
+var nome2 = 'js';
+
+const ok = true;
+
+
+if (ok){
+    let nome = 'iara'; // criando
+    console.log(nome);
+    var nome2 = 'teste' // redeclarando
+    if (ok) {
+        let nome = 'juruna'; // criando
+        var nome2 = 'last' // redeclarando
+        console.log(nome);
+    }
+}
+
+console.log(nome2)
