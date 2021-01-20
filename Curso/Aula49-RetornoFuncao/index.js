5c669421ff9a2254360cb5115cbc82ca668258d6
haiklnx <9haik1@gmail.com>
2021-01-20 13:34:38 -0300

    Retorno de função Aula49

---
+++ Curso/Aula49-RetornoFuncao/index.js
@@ -0,0 +1,17 @@
+function criaMultiplicador(multiplicador){
+    //multiplicador pega o escopo maior
+    
+    return function (numero){
+        return numero * multiplicador;
+    };
+};
+
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+
+console.log(duplica(2));
+console.log(triplica(3));
+console.log(quadriplica(4));
