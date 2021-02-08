0a77ade5f25b6ff171f39dd488140ba20b974815
haiklnx <9haik1@gmail.com>
2021-02-08 07:28:18 -0300

    Herança com Classe Aula80

---
+++ Curso/Aula80/index.js
@@ -0,0 +1,41 @@
+// Herança com classe
+
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if(this.ligado) {
+            console.log(`${this.nome} já está ligado!`);
+            return;
+        }
+        this.ligado = true;
+        console.log('Ligado!')
+    }
+
+    desligar () {
+        if(!this.ligado){
+            console.log(`${this.nome} já foi desligado!`);
+            return;
+        }
+        this.ligado = false;
+        console.log('Desligado!')
+    }
+}
+
+// const dispotivo = new DispositivoEletronico('Lâmpada');
+class Smartphone extends DispositivoEletronico {
+    constructor(nome,cor,modelo){
+        super(nome); // Executa o construtor da classe pai DispositivoEletronico
+
+        this.cor = cor;
+        this.modelo = modelo;
+    };
+};
+
+const s1 = new Smartphone('Moto', 'preto', 'G9');
+console.log(s1);
+s1.desligar();
+s1.ligar();
