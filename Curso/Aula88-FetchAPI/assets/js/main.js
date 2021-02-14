d02f5d822b5e1eb47409b704745f95d0b888ce5f
haiklnx <9haik1@gmail.com>
2021-02-14 20:58:51 -0300

    fetch API Aula87

---
+++ Curso/Aula88-FetchAPI/assets/js/main.js
@@ -0,0 +1,32 @@
+// Fetch API
+
+// 1 -> pegar apenas o elemento que contem o link para executar
+document.addEventListener('click', (e) => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(tag === 'a'){
+        e.preventDefault();
+        carregaPagina(el);
+    }
+})
+
+async function carregaPagina(el){
+    try {
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+        
+        if (response.status !== 200) throw new Error('ERRO 404!!')
+        
+        const html = await response.text();
+        loadDados(html);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// -> function mostra o resultado na tela
+function loadDados(response){
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+};
