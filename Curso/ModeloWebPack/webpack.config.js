0bc288532f02373b390a783c9a799c4cb2b65b2b
haiklnx <9haik1@gmail.com>
2021-02-15 07:47:25 -0300

    Adiciona modelo webpack para as aulas

---
+++ Curso/ModeloWebPack/webpack.config.js
@@ -0,0 +1,24 @@
+//webpack
+const path = require('path'); // CommonJS padrao node
+
+module.exports = {
+    mode: 'production', // deixa todo código gerado em uma linha, deixando apenas o essencial
+    entry: './src/index.js', // arquivo que vai ser lido para ser compilado
+    output:{ 
+        path: path.resolve(__dirname, 'public', 'assets', 'js'), // local de saída
+        filename: 'bundle.js', // nome do arquivo de saída
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/, // retira a pasta da verificação
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        },]
+    },
+    devtool: 'source-map', // vai criar um 'mapa' no navegador dizendo qual linha e arquivo original esta sendo executado o comando ou erro
+}
