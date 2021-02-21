532610632c691a6aa06dbb8d2f88c98039e4f5c6
Gabriel <9haik1@gmail.com>
2021-02-20 21:05:53 -0300

    Criando modelo webpack para aulas futuras

--- Curso/ModeloWebPack/webpack.config.js
+++ Curso/ModeloWebPack/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path'); // CommonJS padrao node
 module.exports = {
     mode: 'production', // deixa todo código gerado em uma linha, deixando apenas o essencial
     entry: './src/index.js', // arquivo que vai ser lido para ser compilado
-    output:{ 
+    output: {
         path: path.resolve(__dirname, 'public', 'assets', 'js'), // local de saída
         filename: 'bundle.js', // nome do arquivo de saída
     },
@@ -18,6 +18,9 @@ module.exports = {
                     presets: ['@babel/env']
                 }
             }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
         },]
     },
     devtool: 'source-map', // vai criar um 'mapa' no navegador dizendo qual linha e arquivo original esta sendo executado o comando ou erro
